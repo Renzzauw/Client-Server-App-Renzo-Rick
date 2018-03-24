174caa975c92045bd7755bbf8717dec64b90a69f
Renzzauw <renzo@schindeler.nl>
2018-03-24 23:33:56 +0100

    server zet een database op + code changes

--- myapp/myapp/app.js
+++ myapp/myapp/app.js
@@ -4,10 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// routes setup
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// express setup
 var app = express();
+
+// fileserver and databases setup
+var fs = require("fs");
+var accountsfile = "databases/accounts.db";
+var accountsexists = fs.existsSync(accountsfile);
+var sqlite3 = require("sqlite3").verbose();
+var accountsdb = new sqlite3.Database(accountsfile);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +28,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// redirection to routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+
+
+// *** error handling *** 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
