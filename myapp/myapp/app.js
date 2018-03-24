43945fafc7515ce908985d63595c30c4821b7566
Renzzauw <renzo@schindeler.nl>
2018-03-25 00:43:05 +0100

    beginnetje inlogscherm

--- myapp/myapp/app.js
+++ myapp/myapp/app.js
@@ -7,16 +7,27 @@ var logger = require('morgan');
 // routes setup
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var signupRouter = require('./routes/signup');
 
 // express setup
 var app = express();
 
 // fileserver and databases setup
 var fs = require("fs");
-var accountsfile = "databases/accounts.db";
-var accountsexists = fs.existsSync(accountsfile);
+var dbfile = "databases/database.db";
+var dbexists = fs.existsSync(dbfile);
 var sqlite3 = require("sqlite3").verbose();
-var accountsdb = new sqlite3.Database(accountsfile);
+var db = new sqlite3.Database(dbfile);
+
+// creates database tables if the file does not exist
+/*
+db.serialize(function() {
+  if(!dbexists) {
+    db.run("CREATE TABLE Accounts (AccountID int, FirstName varchar(255), LastName varchar(255), Username varchar(255), Password varchar(255)); CREATE TABLE Products (ProductID int, ProductName varchar(255), ProductPrice float(255,2), ProductCountInStock int");
+  }
+});
+*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,10 +42,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // redirection to routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-
-
+app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
 
 // *** error handling *** 
 
