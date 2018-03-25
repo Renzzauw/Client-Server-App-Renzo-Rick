f25ff2d2658a7323b1a92f1bb63fc2ed9a4c19d1
Renzzauw <renzo@schindeler.nl>
2018-03-25 17:16:32 +0200

    Ietsje meer inlogbaarheid van inlogscherm

--- myapp/myapp/routes/login.js
+++ myapp/myapp/routes/login.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('login', { title: 'Login' });
+  res.render('login', { title: 'Login' });
+});
+
+/* POST users listing. */
+router.post('/', function(req, res, next) {
+  // get login data from POST request
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log("Handling login");
+  //res.render('login', { title: 'Login' });
+  //res.send("ayy u logged in!");
+  //res.end("Username: " + req.body.password);
+
+  
 });
 
 module.exports = router;
