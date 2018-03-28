b4c0b984147483aff006b33c599f9e9661a57a0f
rickbou11 <rick.boumans98@gmail.com>
2018-03-28 16:14:38 +0200

    start index

--- myapp/myapp/routes/index.js
+++ myapp/myapp/routes/index.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
+var fs = require("fs");
+var file = "databases/database.db";
+var exists = fs.existsSync(file);
+if(!exists) {
+  fs.openSync(file, "w");
+}
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(file);
+
+db.serialize(function() {
+  db.each("SELECT productname,productid FROM Products", function(err,row){
+    document.write(productname);
+  })
+});
+db.close();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
