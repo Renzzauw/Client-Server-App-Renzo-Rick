bb10cbc3183588bb1202cea03a7a555982f1224a
Renzo Schindeler <renzo@schindeler.nl>
2018-04-01 20:27:33 +0200

    begin producten op home page + beginnetje style andere pagina's

--- myapp/myapp/routes/index.js
+++ myapp/myapp/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var $ = require("jquery");
 
 var fs = require("fs");
 var file = "databases/database.db";
@@ -10,6 +9,44 @@ if(!exists) {
 }
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
+
+var field = document.getElementById("product-list");
+var products = [];
+var orderMode = [];
+
+class Product {
+  constructor(productid, productName, releaseDate, publisher, genre){
+    this.productid = productid;
+    this.productName = productName;
+    this.releaseDate = releaseDate;
+    this.publisher = publisher;
+    this.genre = genre;
+    products.push(this);
+  }
+
+  generateProductHtml() {
+    //var field = document.getElementById("product-list");
+    var html = "";
+    html = '<section class="product-field"><h3>Product ID: '+productid+'</h3><h3>'+productName+'</h3><h4>'+releaseDate+' - '+publisher+' - '+genre+'</h4><h3>€'+price+'</h3><img src="'+productImageLocation+'" alt="game avatar"><form><button type="submit">Buy</button></form></section>';
+    return html;
+  }
+}
+
+function getProductsFromDB(){
+  db.serialize(function() {
+    // order by productname
+    db.each("SELECT * FROM Products ORDER BY productname ASC", function(err, row) {
+           pr = new Product(row.productid, row.productname, row.releasedate, row.publisher, row.genre);
+           products.push(pr);      
+    });
+
+    // order by price
+    db.each("SELECT * FROM Products ORDER BY price ASC", function(err, row) {
+      pr = new Product(row.productid, row.productname, row.releasedate, row.publisher, row.genre);
+      products.push(pr);      
+});
+  });
+}
 
 db.serialize(function() {
   db.each("SELECT productname,productid FROM Products", function(err,row){
