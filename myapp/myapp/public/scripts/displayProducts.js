31fb3dbc4b22f03a1b7d6bb0db28ecfb5bb47809
Renzzauw <renzo@schindeler.nl>
2018-04-14 19:04:03 +0200

    Filters veld heeft nu een scrollbalk

--- myapp/myapp/public/scripts/displayProducts.js
+++ myapp/myapp/public/scripts/displayProducts.js
@@ -1,61 +1,77 @@
+var genreCheckBoxes = [];
+var publisherCheckBoxes = [];
+var checkGenresQueryJoined = "";
+var checkedPublishersQueryJoined = "";
 
-/*
-// add event "onchange" listener to sorting menu
-$("#sort").change(function (){
-    $("#catalogue").empty().load("/products?sort=" + $("#sort").val());
-});*/
-
-// load checkboxes for each genre and publisher
-$("#genre-checkboxes").load("/genres");
-$("#publisher-checkboxes").load("/publishers");
-
-// get all checkboxes and check if they are checked
-var checkboxList = $("[type=checkbox]");
-var checkQueries = "";
-console.log(checkboxList);
-
-// add event listeners to each checkbox
-for (var j = 0; j < checkboxList.length; j++) {
-    checkboxList[j].change(function(){
-        checkQueries = "";
-        // generate query segments for each checkbox
-        for (var i = 0; i < checkboxList.length; i++){
-            if (checkboxList[i].is(':checked') && i === checkboxList.length - 1){
-                checkQueries += checkboxList[i].attr('id') + "=true";
-            }    
-            else if (checkboxList[i].is(':checked')){
-                checkQueries += checkboxList[i].attr('id') + "=true&";
-            }
-            else if (i === checkboxList.length() - 1) {
-                checkQueries += checkboxList[i].attr('id') + "=false";
-            }
+var checkedGenresQuery;
+$("#genre-checkboxes").load("/genres", function() {
+    genreCheckBoxes = $(".genrecheckboxes");
+    $("#genre-checkboxes").on('click', function(){
+        checkedGenresQuery = [];
+        for (var i = 0; i < $(".genrecheckboxes").length; i++){
+            if (genreCheckBoxes[i].checked === true) {
+                checkedGenresQuery.push($(".genrecheckboxes")[i].id + "=true");
+            } 
             else {
-                checkQueries += checkboxList[i].attr('id') + "=false&";
+                checkedGenresQuery.push($(".genrecheckboxes")[i].id + "=false");
             }
         }
-        console.log(checkQueries);
-    })
-}
+        checkGenresQueryJoined = checkedGenresQuery.join("&").replace(' ','_');
+        console.log(checkGenresQueryJoined);
+    });
+});
+
+var checkedPublishersQuery;
+$("#publisher-checkboxes").load("/publishers", function() {
+    publisherCheckBoxes = $(".publishercheckboxes");
+    $("#publisher-checkboxes").on('click', function(){
+        checkedPublishersQuery = [];
+        for (var i = 0; i < $(".genrecheckboxes").length; i++){
+            if (publisherCheckBoxes[i].checked === true) {
+                checkedPublishersQuery.push($(".publishercheckboxes")[i].id + "=true");
+            } 
+            else {
+                checkedPublishersQuery.push($(".publishercheckboxes")[i].id + "=false");
+            }
+        }
+        checkedPublishersQueryJoined = checkedPublishersQuery.join("&").replace(' ','_');  
+        console.log(checkedPublishersQueryJoined);
+    });
+});
+
 
 // disable submitting when using the search bar
 $("#search-form").submit(false);
 
 // add even "onsubmit" listener to the search form
 $("#search-bar").on('input', function(event){
-    $("#catalogue").load("/products?sort=" + $("#sort").val() + "&search=" + $("#search-bar").val() + "&" + checkQueries);
+    $("#catalogue").load("/products?sort="                      
+                        + $("#sort").val()                      // sort mode
+                        + "&search=" + $("#search-bar").val()   // searchbar value
+                        + "&" + checkGenresQueryJoined          // genres checkboxes
+                        + "&" + checkedPublishersQueryJoined    // publishers checkboxes
+                        + "&min=" + $("#price-min").val()       // minimum price range
+                        + "&max=" + $("#price-max").val());     // maximum price range
 });
 
 // Send search configuration to server and load products
 $('#filters-form').change(function(){
-    $("#catalogue").load("/products?sort=" 
+    $("#catalogue").load("/products?sort="                      
                         + $("#sort").val()                      // sort mode
                         + "&search=" + $("#search-bar").val()   // searchbar value
-                        + "&" + checkQueries                    // all checkbox restriction values
+                        + "&" + checkGenresQueryJoined          // genres checkboxes
+                        + "&" + checkedPublishersQueryJoined    // publishers checkboxes
                         + "&min=" + $("#price-min").val()       // minimum price range
                         + "&max=" + $("#price-max").val());     // maximum price range
 });
 
 // initialize product sorting with alphabetical order when initially loading index page
-$("#catalogue").load("/products?sort=alphabet");
+$("#catalogue").load("/products?sort="                      
+                        + $("#sort").val()                      // sort mode
+                        + "&search=" + $("#search-bar").val()   // searchbar value
+                        + "&" + checkGenresQueryJoined          // genres checkboxes
+                        + "&" + checkedPublishersQueryJoined    // publishers checkboxes
+                        + "&min=" + $("#price-min").val()       // minimum price range
+                        + "&max=" + $("#price-max").val());     // maximum price range
 
 
